d409803863a111e3e91a52cbade0dc9062fe1832
Tosie <minnaji@outlook.com>
2022-03-15 21:44:35 -0400

    assignment complete

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let result
+  if (someValue < 400) {
+    result = 'This one is on me!';
+  }
+  else if (someValue > 2000 && someValue <2499) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if (someValue > 2500) {
+    result = 'No can do.'
+  }
+ return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  
+   return (city === 'NYC'? 'Ok, sounds good.': "No go.");
+   
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+  
+    case 'generous' :
+      return 'Thank you so much.';
+      break;
+    case 'not as generous' :
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.'
+
+  }
 }
